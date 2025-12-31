e90d1593633c9c4b9f52f8f708ca480642923eb9
Jonathan Wight <jwight@mac.com>
2025-12-30 17:01:28 -0800

    Treat warnings as errors and refactor error handling
    - Enabled GCC_TREAT_WARNINGS_AS_ERRORS in project settings.
    - Updated error handling to replace ImmersiveRendererError with MetalSprocketsError.
    - Removed deprecated ImmersiveRendererError.
    - Added import of MetalSprocketsSupport.

--- Sources/MetalSprocketsUI/VisionOS/Support.swift
+++ Sources/MetalSprocketsUI/VisionOS/Support.swift
@@ -11,11 +11,6 @@ internal actor ImmersiveRendererActor {
     static let shared = ImmersiveRendererActor()
 }
 
-@available(*, deprecated, message: "Use error from MetalSprocket")
-internal enum ImmersiveRendererError: Error {
-    case resourceCreationFailed(String)
-}
-
 internal extension LayerRenderer.Clock.Instant.Duration {
     var toTimeInterval: TimeInterval {
         let nanoseconds = TimeInterval(components.attoseconds / 1_000_000_000)
